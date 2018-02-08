dd2302f846f2a9003b12795e2f5c927c739811e9
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-08 21:23:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var removeLastArray = array.slice(-1);
+  var removeLastArray = array.slice(0, array.length-1);
   return removeLastArray;
 }
